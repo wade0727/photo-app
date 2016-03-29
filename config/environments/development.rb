8716fc28a7c826c80182c156ff5e441c77e019ac
Matt Wade <mdwade@gmail.com>
2016-03-29 01:21:31 +0000

    corrected error in production.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { :host => 'https://rails-projects-mwade.c9users.io'}
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -10,6 +8,8 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'https://rails-projects-mwade.c9users.io'}
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'stormy-citadel-70321.herokuapp.com', :protocol => 'https'}
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -11,6 +9,9 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'stormy-citadel-70321.herokuapp.com', :protocol => 'https'}
+
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
