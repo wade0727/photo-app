e22f4efb23313e1af982889c2646a248ec957266
Matt Wade <mdwade@gmail.com>
2016-03-29 01:07:38 +0000

    Add Email Authentication and styling to nav bar

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'https://rails-projects-mwade.c9users.io'}
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
